3d8f813263ac603a396953f97524101ff6e0b7c7
liuchengyuan <liucy@rp-pet.cn>
2020-09-18 08:37:16 +0800

    add

--- view/src/base/6.js
+++ view/src/base/6.js
@@ -1,6 +1,40 @@
 /**
- 
-
-
-
  */
+
+function throttle(callback, time, context) {
+  let timer
+  let lastTime
+  let dist
+  return function (args) {
+    if (!lastTime) {
+      lastTime = +new Date()
+      return callback.call(context, ...args)
+    } else {
+      dist = +new Date() - lastTime
+      if (dist >= time) {
+        lastTime = +new Date()
+        return callback.call(context, ...args)
+      }
+      if (timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(() => {
+        callback.call(context, ...args)
+      }, time - dist)
+    }
+  }
+}
+
+function debounce(callback, time, context) {
+  let timer
+  return function (args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      callback.call(context, ...args)
+    }, time)
+  }
+}
+
+// Promise
