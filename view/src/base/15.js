62e5841ef4e775288a33fc753f99546bb3e986f6
liuchengyuan <liucy@rp-pet.cn>
2020-09-19 17:27:56 +0800

    add

--- view/src/base/15.js
+++ view/src/base/15.js
@@ -22,7 +22,7 @@ function Observer(data) {
     },
     get(obj, prop) {
       // console.log('get', obj, prop)
-      if (!DepMap.has(prop)) {
+      if (!DepMap.has(prop) && prop in obj) {
         DepMap.set(prop, new Dep())
       }
       let dep = DepMap.get(prop)
@@ -34,6 +34,14 @@ function Observer(data) {
         dep.depend(Dep.target)
       }
       return value
+    },
+    deleteProperty(obj, prop) {
+      delete obj[prop]
+      let dep = DepMap.get(prop)
+      if (dep) {
+        dep.notify()
+      }
+      console.log('deleteProperty', obj, prop)
     }
   })
 }
@@ -44,6 +52,13 @@ class Dep {
   }
   depend(effect) {
     this.subscribe.push(effect)
+    effect.depend(this)
+  }
+  cancel(effect) {
+    let index = this.subscribe.indexOf(effect)
+    if (index > -1) {
+      this.subscribe.splice(index, 1)
+    }
   }
   notify() {
     let arr = this.subscribe.slice()
@@ -56,20 +71,32 @@ class Dep {
     }
   }
 }
+Dep.target = null
 
 function effect(callback) {
   if (typeOf(callback) === 'Function' && !callback._effect) {
     const effectObj = {
-      value: undefined
+      value: undefined,
+      deps: [],
+      depend(dep) {
+        this.deps.push(dep)
+      },
+      cancel() {
+        this.deps.forEach((dep) => {
+          dep.cancel(this)
+        })
+        this.deps = []
+      }
     }
     callback._effect = effectObj
     effectObj.exec = callback
     Dep.target = effectObj
     callback()
   }
+  return () => {
+    callback._effect.cancel()
+  }
 }
-
-Dep.target = null
 
 let p = Observer({
   a: 1,
@@ -77,12 +104,11 @@ let p = Observer({
     c: 1
   }
 })
-
-effect(() => {
+let cancel = effect(() => {
   console.log('effect ===> p.a', p.a)
 })
-// console.log('p.b.c', p.b.c)
-
+// // console.log('p.b.c', p.b.c)
+// // cancel()
 setTimeout(() => {
-  p.a = 123
+  delete p.a
 }, 2000)
